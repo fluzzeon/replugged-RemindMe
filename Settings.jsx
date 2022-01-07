a2c88579dd2ccfc1ebf2a1e0a73c5af24c28f37e
dcrime <ppp.dddd7@gmail.com>
2022-01-07 21:40:21 +0100

    Refactor, ability to remove reminders, snooze
    Refactored the code in hopes of making it easier to read
    
    Added a faulty system to tell you how long until your reminder
    
    Added the ability to remove a reminder or to remove all reminders
    
    Added a snooze button which snoozes for 5 min
    
    Hopefully I caught most of my bugs

--- Settings.jsx
+++ Settings.jsx
@@ -27,35 +27,15 @@ module.exports = class Settings extends React.Component {
             onChange={(o) => {
               updateSetting("reminderSound", o.value);
 
-              let sound;
-              switch (getSetting("reminderSound", 0)) {
-                case 0:
-                  sound = new Audio(
-                    "/assets/15fe810f6cfab609c7fcda61652b9b34.mp3"
-                  );
-                  break;
-                case 1:
-                  sound = new Audio(
-                    "/assets/53ce6a92d3c233e8b4ac529d34d374e4.mp3"
-                  );
-                  break;
-                case 2:
-                  sound = new Audio(
-                    "/assets/fa4d62c3cbc80733bf1f01b9c6f181de.mp3"
-                  );
-                  break;
-                case 3:
-                  sound = new Audio(
-                    "/assets/a5f42064e8120e381528b14fd3188b72.mp3"
-                  );
-                  break;
-                case 4:
-                  sound = new Audio(
-                    "/assets/ad322ffe0a88436296158a80d5d11baa.mp3"
-                  );
-                  break;
-              }
-              sound.play();
+              new Audio(
+                [
+                  "/assets/15fe810f6cfab609c7fcda61652b9b34.mp3",
+                  "/assets/53ce6a92d3c233e8b4ac529d34d374e4.mp3",
+                  "/assets/fa4d62c3cbc80733bf1f01b9c6f181de.mp3",
+                  "/assets/a5f42064e8120e381528b14fd3188b72.mp3",
+                  "/assets/ad322ffe0a88436296158a80d5d11baa.mp3"
+                ][getSetting("reminderSound", 0)]
+              ).play();
             }}
           >
             Reminder Sound
